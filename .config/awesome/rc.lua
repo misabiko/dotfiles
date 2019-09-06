1b48cb4a5fd18461d92e702178d893f63b43efb3
misabiko <misabiko@gmail.com>
2019-09-06 02:52:43 -0400

    Trying out a wallpaper

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -51,6 +51,7 @@ beautiful.titlebar_close_button_normal = "/usr/share/awesome/themes/cesious/titl
 beautiful.titlebar_close_button_focus = "/usr/share/awesome/themes/cesious/titlebar/close_focus_adapta.png"
 beautiful.font              = "Noto Sans Regular 10"
 beautiful.notification_font = "Noto Sans Bold 14"
+beautiful.wallpaper = "/home/misabiko/.config/awesome/mikael-gustafsson-wallpaper-mikael-gustafsson.jpg"
 
 -- This is used later as the default terminal and editor to run.
 browser = "exo-open --launch WebBrowser" or "vivaldi-stable"
