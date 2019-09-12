4917333954eb90998f1232d9414c37c258b19801
misabiko <misabiko@gmail.com>
2019-09-12 08:10:05 -0400

    Removed awesome (replace with submodule)

--- .config/awesome/main/user-variables.lua
+++
@@ -1,7 +0,0 @@
-return {
-	terminal = os.getenv("terminal") or "lxterminal",
-	browser = "exo-open --launch WebBrowser" or "vivaldi-stable",
-	filemanager = "exo-open --launch FileManager" or "thunar",
-	gui_editor = "atom",
-	modkey = "Mod4"
-}
