e77c30fe2e47361f9fb093301735e98f9381885c
misabiko <misabiko@gmail.com>
2019-09-11 23:41:29 -0400

    Transparent wibox

--- .config/awesome/main/theme.lua
+++ .config/awesome/main/theme.lua
@@ -5,13 +5,16 @@ local beautiful = require("beautiful")
 
 beautiful.init("/usr/share/awesome/themes/cesious/theme.lua")
 beautiful.icon_theme        = "Papirus-Dark"
-beautiful.bg_normal         = "#222D32"
-beautiful.bg_focus          = "#2C3940"
+beautiful.bg_normal         = "#22222255"
+beautiful.bg_systray          = beautiful.bg_normal
+beautiful.bg_focus          = "#535d6c80"
+beautiful.bg_urgent     	= beautiful.bg_focus
+beautiful.bg_minimize   	= "#44444455"
 beautiful.titlebar_close_button_normal = "/usr/share/awesome/themes/cesious/titlebar/close_normal_adapta.png"
 beautiful.titlebar_close_button_focus = "/usr/share/awesome/themes/cesious/titlebar/close_focus_adapta.png"
 beautiful.font              = "Noto Sans Regular 10"
 beautiful.notification_font = "Noto Sans Bold 14"
-beautiful.wallpaper = "usr/share/backgrounds/mikael-gustafsson-wallpaper-mikael-gustafsson.jpg"
+beautiful.wallpaper = "/usr/share/backgrounds/mikael-gustafsson-wallpaper-mikael-gustafsson.jpg"
 
 local function set_wallpaper(s)
     -- wallpaper
